bf2f77d1663c9182c8508cf2eebd09a5613c5d30
ljunseok0514 <dlwnstjr456@naver.com>
2026-06-27 11:31:01 +0900

    260627 useEffect styudy

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,46 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
+
+let NewBtn = styled(YellowBtn)`
+  color: red;
+`;
+
+let Box = styled.div`
+  background: grey;
+  padding: 20px;
+`;
 
 function Detail(props) {
+  useEffect(() => {
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 2000);
+  });
+  let [count, setCount] = useState(0);
+
   let { id } = useParams();
   let result = props.shoes.find((item) => item.id === Number(id));
   console.log(result);
   return (
     <div className="container">
+      <div className="alert alert-warning"> 2초이내 구매시 할인</div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
+      <Box>
+        <NewBtn>버튼</NewBtn>
+      </Box>
       <div className="row">
         <div className="col-md-6">
           <img
